f5686975073f4b34437fc724db13ce9a89996c4c
Anubhav Goyal <anubhavgoyal0504@gmail.com>
2026-06-17 21:58:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Amazone from './Amazone';
+import './App.css'
+/* 
+import {usestate} from 'react'
+import Demo from './component/Demo';
+import Props from './component/Props'; */
+
+function App() {
+
+  const name="React JS1";
+  return (
+    <>
+      {/* <h1>Hello {name}</h1> */}
+      {/* <Demo/> */}
+      {/* <Props name="Anuj" rollno="2503201000247" course="Btech"/> */}
+ <Amazone/>
+
+    </>
+  )
+}
+
+export default App 
